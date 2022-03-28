937622f67b91598527191db8bed78c83024b29cb
dredd404 <59948599+dredd404@users.noreply.github.com>
2022-03-28 11:38:29 +1000

    Add files via upload

---
+++ What_1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+    cout << pow(a,2); //Возведение в степень
+    return 0;
+}
---
+++ What_2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+    cout << pow(a,3); //Возведение в степень 3
+    return 0;
+}
